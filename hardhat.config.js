754060095530af63f0197c611f7dcac540f67c4c
William Chargin <wchargin@gmail.com>
2022-07-06 11:06:43 -0700

    hardhat: add `devdoc` and `userdoc` outputs

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["devdoc", "userdoc"],
+        },
+      },
+    },
+  },
 };
